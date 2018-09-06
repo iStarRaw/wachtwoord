085f631ab6ebd2afd8df9d7ef93c5a7ed6e79cd9
Ilsa <35494284+iStarRaw@users.noreply.github.com>
2018-09-06 13:39:45 +0200

    initial

---
+++ src/main/java/ilsa/wachtwoord/App.java
@@ -0,0 +1,13 @@
+package ilsa.wachtwoord;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
