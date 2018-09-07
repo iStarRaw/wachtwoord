08d0aeb840e2a00abca8e1a22f9bf5117c188eae
Ilsa <35494284+iStarRaw@users.noreply.github.com>
2018-09-07 09:15:32 +0200

    addCharToPassword methode gemaakt en 1e twee chars toegevoegd

--- src/main/java/ilsa/wachtwoord/models/WachtwoordGenerator.java
+++ src/main/java/ilsa/wachtwoord/models/WachtwoordGenerator.java
@@ -1,16 +1,100 @@
 package ilsa.wachtwoord.models;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WachtwoordGenerator {
 	private final String CANDIDATE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%~_^&*/(/)///-/+/=/[/]/{/}/|/`?><.,";
 	private SecureRandom secGenerator = new SecureRandom();
 	
-	
-	
-	
-	
-	
+	private List<Character> password;
+
+	public String generatePassword() {
+		
+		createPassword();
+		
+		addCharToPassword(); // 1e character
+		
+		addCharToPassword(); // 2e character				
+
+
+		generateThirdChar();
+		addCharToList(); // 3e character
+
+		while (containsSequence()) {
+			replaceLastChar();
+		}
+
+		while (containsTriplicates()) {
+			replaceLastChar();
+		}
+
+		generateFourthTillEightChar();
+		addCharToList(); // 4e character
+		
+		while (containsSequence()) {
+			replaceLastChar();
+		}
+
+		while (containsTriplicates()) {
+			replaceLastChar();
+		}
+		
+		generateFourthTillEightChar();
+		addCharToList(); // 5e character
+		
+		while (containsSequence()) {
+			replaceLastChar();
+		}
+
+		while (containsTriplicates()) {
+			replaceLastChar();
+		}
+		
+		generateFourthTillEightChar();
+		addCharToList(); // 6e character
+		
+		while (containsSequence()) {
+			replaceLastChar();
+		}
+
+		while (containsTriplicates()) {
+			replaceLastChar();
+		}
+		
+		generateFourthTillEightChar();
+		addCharToList(); // 7e character
+		
+		while (containsSequence()) {
+			replaceLastChar();
+		}
+
+		while (containsTriplicates()) {
+			replaceLastChar();
+		}
+		
+		generateFourthTillEightChar();
+		addCharToList(); // 8e character
+		
+		while (containsSequence()) {
+			replaceLastChar();
+		}
+
+		while (containsTriplicates()) {
+			replaceLastChar();
+		}
+		
+//		String str = password.stream().map(e -> e.toString()).collect(Collectors.joining());
+//		System.out.println(str);
+//		return str;
+		return null;
+
+	}
+
+	private void createPassword() {
+		password = new ArrayList<>();
+	}
 	
 	/**
 	 * Generates one random char. Extended ASCII: 0 - 255
@@ -21,5 +105,105 @@ public class WachtwoordGenerator {
 		char randomChar = CANDIDATE_CHARS.charAt(secGenerator.nextInt(CANDIDATE_CHARS.length()));
 		return randomChar;
 	}
+	
+	/**
+	 * Adds a char to the list
+	 */
+	private void addCharToPassword() {
+		password.add(generateChar());
+		
+	}
+
+	private void generateFourthTillEightChar() {
+		// TODO No switch or ifs: better map or polymorphism
+		
+	}
+
+	private boolean lastThreeAllSymbol() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean lastThreeAllDigit() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean lastThreeAllLetter() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	private void replaceLastChar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean containsTriplicates() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean containsSequence() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	private void generateThirdChar() {
+		// TODO No switch or ifs: better map or polymorphism
+		if (lastTwoAllLetter()) {
+			generateLetterChar();
+		} else if (lastTwoAllDigit()) {
+			generateDigitChar();
+		} else if (lastTwoAllSymbol()) {
+			generateSymbolChar();
+		} else {
+			generateChar();
+		}
+
+	}
+
+	private void generateSymbolChar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void generateDigitChar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void generateLetterChar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean lastTwoAllSymbol() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean lastTwoAllDigit() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean lastTwoAllLetter() {
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+	
+	
 
 }
